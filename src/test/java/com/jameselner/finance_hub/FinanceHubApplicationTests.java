9ae15f8d2c781304f72cf0823c513fdaa364047b
James Elner <elner.k.james@gmail.com>
2025-11-21 20:29:08 +0000

    Initial commit

---
+++ src/test/java/com/jameselner/finance_hub/FinanceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jameselner.finance_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
