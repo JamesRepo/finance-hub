0a254db2936f341442fcff551224c22fab85599f
James Elner <elner.k.james@gmail.com>
2026-01-11 21:39:55 +0000

    Add favicon

---
+++ src/main/java/com/jameselner/finance_hub/AppShellConfig.java
@@ -0,0 +1,8 @@
+package com.jameselner.finance_hub;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
+@PWA(name = "Health Hub", shortName = "Health Hub")
+public class AppShellConfig implements AppShellConfigurator {
+}
